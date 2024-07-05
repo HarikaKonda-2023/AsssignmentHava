66c14f2ad49ddb85e4f5b27127da2419d997eadd
HarikaKonda-2023 <145096439+HarikaKonda-2023@users.noreply.github.com>
2024-07-05 11:47:18 +0530

    Create index.js

---
+++ src/components/HeaderCon/index.js
@@ -0,0 +1,11 @@
+const HeaderCon = () => {
+  ;<div className="header-con">
+    <h1>Hava Havai</h1>
+    <img
+      src="https://res.cloudinary.com/du3qvpyzd/image/upload/v1720157987/Avatar_-_Desktop_-_Light_aq73bu.png"
+      className="logo-con"
+    />
+  </div>
+}
+
+export default HeaderCon
