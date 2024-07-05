c250197fc75a5927b304a57cae89099fbe208778
HarikaKonda-2023 <145096439+HarikaKonda-2023@users.noreply.github.com>
2024-07-05 11:44:38 +0530

    Update index.js

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,1 +1,14 @@
+import HeaderCon from '../HeaderCon'
+import SideBarCon from '../SideBarCon'
+import MainCon from '../MainCon'
+
+const HomePage = () => {
+  ;<div className="main-con">
+    <HeaderCon />
+    <div className="sub-main-con">
+      <SideBarCon />
+      <MainCon />
+    </div>
+  </div>
+}
 
